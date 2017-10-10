1be963dba3d540323cab66660808223605c5c204
Tien Pham <r3dsky02@yahoo.com.vn>
2017-10-10 13:14:40 +0300

    Testing out tfidf + changing to webpack

---
+++ tfidf.js
@@ -0,0 +1,18 @@
+var natural = require('natural');
+var TfIdf = natural.TfIdf;
+var tfidf = new TfIdf();
+
+tfidf.addDocument('this document is about node.');
+tfidf.addDocument('this document is about ruby.');
+tfidf.addDocument('this document is about ruby and node.');
+tfidf.addDocument('this document is about node. it has node examples');
+
+console.log('node --------------------------------');
+tfidf.tfidfs('node', function(i, measure) {
+    console.log('document #' + i + ' is ' + measure);
+});
+
+console.log('ruby --------------------------------');
+tfidf.tfidfs('ruby', function(i, measure) {
+    console.log('document #' + i + ' is ' + measure);
+});
